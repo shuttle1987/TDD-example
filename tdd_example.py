dfcbb6fa1e371b4bd22a9ab45c3ceea1696673cd
Janis <janis@customprogrammingsolutions.com>
2019-01-21 02:41:48 +1100

    Implement newer requirement with type restriction

--- tdd_example.py
+++ tdd_example.py
@@ -1,3 +1,5 @@
 def multiply_two(first, second):
     """Multiply the two arguments together"""
+    if isinstance(first, str) or isinstance(second, str):
+        raise TypeError("Strings are not allowed")
     return first * second
