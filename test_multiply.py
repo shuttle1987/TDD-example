7613282e59bd92fbbe3fea234d1ad0bac9ff2e01
Janis <janis@customprogrammingsolutions.com>
2019-01-21 02:38:58 +1100

    Implement test for new requirement

--- test_multiply.py
+++ test_multiply.py
@@ -3,3 +3,12 @@ def test_multiply_function():
 
     assert multiply_two(1, 4) == 4
     assert multiply_two(0, 3) == 0
+
+def test_multiply_function_no_strings():
+    """Test that attempting to use the multiply function with
+    a string as an argument raises TypeError"""
+    from tdd_example import multiply_two
+
+    import pytest
+    with pytest.raises(TypeError):
+        multiply_two("abc", 2)
